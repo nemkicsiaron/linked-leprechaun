1154cee86783246967d22c44446f81a8fd216af5
nemkicsiaron <aronfszabo@gmail.com>
2023-09-06 22:29:30 +0200

    trying to remove content stuff

---
+++ scripts/process.js
@@ -0,0 +1,24 @@
+let data;
+// console.log(data);
+// (async () => {
+//     const src = chrome.runtime.getURL("your/content_main.js");
+//     const collected_data = await import(src);
+//     data = collected_data.collect();
+// })();
+
+chrome.runtime.onMessage.addListener((req, sender, sendResponse) => {
+    if(req.type === 'collect') {
+        console.log(req);
+        data = collect();
+        sendResponse({type: 'thanks', message: 'tysm'});
+    }
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector("button#generate").addEventListener("click", () => alert(data), false);
+}, false);
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector("button#options").addEventListener("click", () => chrome.runtime.openOptionsPage(), false);
+}, false);
